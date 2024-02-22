ae1d59dd43a2cb0beac7fbe1b4936d2127419770
Calvin <calvinsoares17@gmail.com>
2024-02-22 14:08:47 -0300

    feat: renderização dos componentes react

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
-import Image from "next/image";
+import { React } from 'react'
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import FooterSection from "./components/FooterSection";
+
 
 export default function Home() {
+
+
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4 ">
       <NavBar />
@@ -12,7 +17,9 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
-      </div>         
+        <EmailSection /> 
+      </div>   
+        <FooterSection />  
     </main>
   );
 }
