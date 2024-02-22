8a767343aaaed7b80abf00a182705f9312b4f470
Calvin <calvinsoares17@gmail.com>
2024-02-22 14:08:05 -0300

    feat: raiz do projeto

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import React from "react";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,8 +12,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <React.StrictMode>
+      <html lang="en">
+        <head>
+          <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+          <link rel="icon" href="/favicon.ico" sizes="20" />
+        </head>
       <body suppressHydrationWarning={true} className={inter.className}>{children}</body>
     </html>
+    </React.StrictMode>   
   );
 }
