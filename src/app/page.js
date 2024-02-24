817bc59eb2c218984c9f17d1e0d1f1dab8d3fe58
Calvin <calvinsoares17@gmail.com>
2024-02-23 21:52:29 -0300

    styled: responsividade como mobile

--- src/app/page.js
+++ src/app/page.js
@@ -11,9 +11,9 @@ export default function Home() {
 
 
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4 ">
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-6 py-4 ">
       <NavBar />
-      <div className="container mt-24 mx-auto px-12 py-12">
+      <div className="container mt-24 mx-auto px-4 py-12">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
